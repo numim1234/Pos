2751449e4607d700973a786886cd69c02541f5d2
numnim1234 <na16972@sansai.ac.th>
2025-05-08 10:14:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap 5 Template</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap Icons -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="css/Product.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+  <!-- Headers!!!! -->
+
+  <header>
+    <div class="px-3 py-2 text-bg-success  border-bottom">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
+          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+            Pos
+          </a>
+
+
+
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li><a href="#" class="nav-link text-white"><i class="bi bi-house d-block mx-auto mb-1"></i>Home</a></li>
+            <li><a href="#" class="nav-link text-white"><i
+                  class="bi bi-speedometer2 d-block mx-auto mb-1"></i>Dashboard</a></li>
+            <li><a href="#" class="nav-link text-white"><i class="bi bi-table d-block mx-auto mb-1"></i>Orders</a></li>
+            <li><a href="#" class="nav-link text-white"><i class="bi bi-grid d-block mx-auto mb-1"></i>Products</a></li>
+            <li><a href="#" class="nav-link text-white"><i class="bi bi-people d-block mx-auto mb-1"></i>Customers</a>
+            </li> 
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Heroes!!! -->
+    <div class="px-3 py-2 border-bottom mb-3">
+      <div class="container d-flex flex-wrap justify-content-center">
+        <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+        <div class="text-end">
+          <a href="login.php" class="btn btn-light text-bs-danger me-2" aria-current="page">Active link</a>
+          <button type="button" class="btn btn-primary">Sign-up</button>
+          
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6"> <img
+          src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-2240-622f-a87d-b88aa382b470/raw?se=2025-05-07T08%3A09%3A43Z&sp=r&sv=2024-08-04&sr=b&scid=8e0b2894-9a0f-55ca-88db-5e71ae5bae1f&skoid=de76bc29-7017-43d4-8d90-7a49512bae0f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-07T02%3A16%3A45Z&ske=2025-05-08T02%3A16%3A45Z&sks=b&skv=2024-08-04&sig=CFwhDQlYdKemMA/Jz8Fnv1WW82YikXmUGwRafKpZN48%3D"
+          class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"> </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">ระบบ Pos คืออะไร ?</h1>
+        <p class="lead">ความหมายและความสำคัญของระบบ POS (Point of Sale)
+          ระบบ POS หรือ Point of Sale คือ ระบบบริหารจัดการการขายหน้าร้าน ที่ถูกพัฒนาขึ้นมาเพื่อช่วยให้ธุรกิจค้าปลีก ร้านอาหาร และร้านค้าที่มีหน้าร้าน สามารถทำงานได้อย่างมีประสิทธิภาพ ครอบคลุมตั้งแต่กระบวนการรับออร์เดอร์จากลูกค้า การคำนวณราคาสินค้าหรือบริการ การพิมพ์ใบเสร็จรับเงิน ตลอดจนการจัดการสต็อกสินค้า การจัดเก็บข้อมูลการขาย และการวิเคราะห์ข้อมูลต่าง ๆ เพื่อใช้ในการบริหารร้าน
+          
+          ปัจจุบันระบบ POS ไม่ใช่แค่เครื่องคิดเงินเท่านั้น แต่ได้ถูกพัฒนาให้มีฟังก์ชันการทำงานที่หลากหลาย และสามารถเชื่อมต่อกับอุปกรณ์อื่น ๆ ได้ เช่น เครื่องพิมพ์ใบเสร็จ เครื่องสแกนบาร์โค้ด ลิ้นชักเก็บเงิน และระบบการชำระเงินแบบอิเล็กทรอนิกส์ (e-Payment) นอกจากนี้ยังสามารถใช้งานได้ทั้งบนคอมพิวเตอร์ แท็บเล็ต และสมาร์ตโฟน ผ่านระบบคลาวด์ (Cloud-Based POS) ซึ่งช่วยให้เจ้าของธุรกิจสามารถเข้าถึงข้อมูลร้านค้าได้ทุกที่ทุกเวลา </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start"> <button type="button"
+            class="btn btn-primary btn-lg px-4 me-md-2">Primary</button> <button type="button"
+            class="btn btn-outline-secondary btn-lg px-4">Default</button> </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Features!!!!! -->
+  <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+    <div class="col">
+      <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+        style="background-image: url('unsplash-photo-1.jpg');">
+        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
+          <ul class="d-flex list-unstyled mt-auto">
+            <li class="me-auto"> <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32"
+                class="rounded-circle border border-white"> </li>
+            <li class="d-flex align-items-center me-3"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Location">
+                <use xlink:href="#geo-fill"></use>
+              </svg> <small>Earth</small> </li>
+            <li class="d-flex align-items-center"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Duration">
+                <use xlink:href="#calendar3"></use>
+              </svg> <small>3d</small> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+        style="background-image: url('unsplash-photo-2.jpg');">
+        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Much longer title that wraps to multiple lines</h3>
+          <ul class="d-flex list-unstyled mt-auto">
+            <li class="me-auto"> <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32"
+                class="rounded-circle border border-white"> </li>
+            <li class="d-flex align-items-center me-3"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Location">
+                <use xlink:href="#geo-fill"></use>
+              </svg> <small>Pakistan</small> </li>
+            <li class="d-flex align-items-center"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Duration">
+                <use xlink:href="#calendar3"></use>
+              </svg> <small>4d</small> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+        style="background-image: url('unsplash-photo-3.jpg');">
+        <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
+          <ul class="d-flex list-unstyled mt-auto">
+            <li class="me-auto"> <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32"
+                class="rounded-circle border border-white"> </li>
+            <li class="d-flex align-items-center me-3"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Location">
+                <use xlink:href="#geo-fill"></use>
+              </svg> <small>California</small> </li>
+            <li class="d-flex align-items-center"> <svg class="bi me-2" width="1em" height="1em" role="img"
+                aria-label="Duration">
+                <use xlink:href="#calendar3"></use>
+              </svg> <small>5d</small> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Product !!!! -->
+  <!-- ตัวที่ 1 -->
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+    <div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <div class="my-3 py-3">
+        <h2 class="display-5">Another headline</h2>
+        <p class="lead">And an even wittier subheading.</p>
+
+      </div>
+      <div class="bg-success shadow-sm mx-auto"
+          style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;background-color: #0f57ca;">
+
+      </div>
+    </div>
+    <!-- ตัวที่ 2 -->
+    <div class="bg-success text-white me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <div class="my-3 py-3">
+        <h2 class="display-5">Products</h2>
+        <p class="lead">And an even wittier subheading.</p>
+      </div>x
+    </div>
+
+  </div>
+
+  
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+     <div class="col-md-4 d-flex align-items-center">
+       <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> 
+        <svg class="bi" width="30" height="24" aria-hidden="true"><use xlink:href="#bootstrap"></use></svg> </a> 
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span> </div> 
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex"> <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Instagram">
+          <svg class="bi" width="24" height="24" aria-hidden="true"><use xlink:href="#instagram"></use></svg></a></li> <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Facebook"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> </ul> 
+        </footer>
+  
+
+  
+
+</body>
+
+</html>
